4b8708e5a334415dc860affbbe9ac5dbe4991c29
suhado <suha@sookmyung.ac.kr>
2022-02-07 10:26:44 +0900

    코드 정리

--- index.js
+++ index.js
@@ -1,54 +1,51 @@
+// (bgm 선택)
+const bgm = document.getElementById('bgm');
+
 // [시작하기] 버튼
-const $startbtn  = document.getElementById('start');
-$startbtn.addEventListener('click', function() {
-$startbtn.style.display = 'none';
+const startbtn  = document.getElementById('start');
 
-const main = document.querySelectorAll(".mainpage");
-for (let i = 0; i < main.length; i++) {
-  const item = main.item(i);
-  item.style.visibility='visible';
-}
+startbtn.addEventListener('click', function() {
+  startbtn.style.display = 'none';
 
-// 시작하기 버튼 누르면 bgm 실행
-const audio = document.getElementById('bgm'); 
-if (audio.paused) { 
-    audio.play(); 
-} else {  
-    audio.pause(); 
-    audio.currentTime = 0 
+  const main = document.querySelectorAll(".mainpage");
+  for (let i = 0; i < main.length; i++) {
+    const item = main.item(i);
+    item.style.visibility='visible';
   }
+
+  // 시작하기 버튼 누르면 bgm 실행
+  bgm.play();
 })
 
-// [soundon/off] 버튼
+// bgm 버튼
 // [soundoff] 버튼
-const $soundoff = document.getElementById('soundoff');
-const $soundon = document.getElementById('soundon');
-$soundoff.addEventListener('click', function() {
-  const audio = document.getElementById('bgm'); 
-  audio.pause();
+const soundoff = document.getElementById('soundoff');
+const soundon = document.getElementById('soundon');
 
-  $soundon.style.display='flex';
-  $soundoff.style.display='none';
+soundoff.addEventListener('click', function() {
+  bgm.pause();
+
+  soundon.style.display='flex';
+  soundoff.style.display='none';
 })
 
 // [soundon] 버튼
-$soundon.addEventListener('click', function() {
-  const audio = document.getElementById('bgm'); 
-  audio.play();
+soundon.addEventListener('click', function() {
+  bgm.play();
 
-  $soundon.style.display='none';
-  $soundoff.style.display='flex';
+  soundon.style.display='none';
+  soundoff.style.display='flex';
 })
 
 // 카드 클릭시
-const $cards = document.querySelectorAll('.card');
-$cards.forEach(function(clickcard) {
+const cards = document.querySelectorAll('.card');
+cards.forEach(function(clickcard) {
   clickcard.addEventListener('click', function() {
   
     // 클릭시 효과음
     const clicksound = document.getElementById('clicksound');
     if (clicksound.paused) {
-      clicksound.play()
+      clicksound.play();
     } else { 
       clicksound.pause(); 
       clicksound.currentTime = 0 
@@ -59,34 +56,46 @@ $cards.forEach(function(clickcard) {
   })
 });
 
+// (메인 화면 선택)
+const main = document.getElementById('main');
+
 // [다시하기] 버튼
-const $restart = document.getElementById('restart');
-$restart.addEventListener('click', function() {
+const restart = document.getElementById('restart');
+restart.addEventListener('click', function() {
 
   const cards = document.querySelectorAll(".card");
   for (let i = 0; i < cards.length; i++) {
     const item = cards.item(i);
     item.classList.remove("clicked")
+
+    // 최종 결과값 초기화
     finalresult = []
   }
 })
 
+
+// (정답 모달 선택)
+const correct = document.getElementById('correct');
+// (오답 모달 선택)
+const wrong = document.getElementById('wrong');
+
 // [정답 확인하기] 버튼
 const answers = ['0', '1', '2', '4', '7'];
+
+// 최종 결과 배열
 let finalresult = [];
 
-
-const $checkanswer = document.getElementById('answer');
-$checkanswer.addEventListener('click', function() {
+const checkanswer = document.getElementById('answer');
+checkanswer.addEventListener('click', function() {
   // 최종 결과값 초기화
   finalresult = [];
 
   // 모달창 뜰 때 클릭 잠금
-  document.getElementById('main').classList.add('clicklock');
+  main.classList.add('clicklock');
 
   // nodeList 배열로 변환
-  const $myanswers = document.querySelectorAll('.clicked');
-  Array.from($myanswers).forEach(function(i) { 
+  const myanswers = document.querySelectorAll('.clicked');
+  Array.from(myanswers).forEach(function(i) { 
     let result = [i.id];
     finalresult = [...finalresult, ...result];
   });
@@ -94,62 +103,50 @@ $checkanswer.addEventListener('click', function() {
     // 정답일 때
     if ((JSON.stringify(finalresult)) == JSON.stringify(answers)) {      
       // 정답일 때 보여줄 모달창
-      const correct = document.getElementById('correct');
       correct.style.display = 'flex';
 
       // 정답 효과음
       const correctsound = document.getElementById('correctsound'); 
-      if (correctsound.paused) { 
-          correctsound.play(); 
-      } else { 
-          correctsound.pause(); 
-          correctsound.currentTime = 0 
-      }
+      correctsound.play(); 
     }
     // 오답일 때
     else {
       // 오답일 때 보여줄 모달창
-      const wrong = document.getElementById('wrong');
       wrong.style.display = 'flex';
 
       // 오답 효과음
       const wrongsound = document.getElementById('wrongsound');
-      if (wrongsound.paused) { 
-          wrongsound.play();
-      } else { 
-          wrongsound.pause(); 
-          wrongsound.currentTime = 0 
-      }
+      wrongsound.play();
     }
+});
+
+
+// 정답일 때 모달창 [닫기 버튼]
+const wrongclose = document.getElementById('wrongclose');
+wrongclose.addEventListener('click', function() {
+  wrong.style.display = 'none';
+
+  // 클릭 잠금 해제
+  main.classList.toggle('clicklock');
 })
 
 
 // 정답일 때 모달창 [닫기 버튼]
-const $wrongclose = document.getElementById('wrongclose');
-const $wrong = document.getElementById('wrong');
-$wrongclose.addEventListener('click', function() {
-  $wrong.style.display = 'none';
-  // 클릭 잠금 해제
-  document.getElementById('main').classList.toggle('clicklock');
-})
+const correctclose = document.getElementById('correctclose');
+correctclose.addEventListener('click', function() {
+  correct.style.display = 'none';
 
-
-// 정답일 때 모달창 [닫기 버튼]
-const $correctclose = document.getElementById('correctclose');
-const $correct = document.getElementById('correct');
-$correctclose.addEventListener('click', function() {
-  $correct.style.display = 'none';
   // 클릭 잠금 해제
-  document.getElementById('main').classList.toggle('clicklock');
+  main.classList.toggle('clicklock');
 })
   
 // [이미지 출처 보기] 토글 버튼
-const $reference = document.getElementById('ref_btn');
-const $ref = document.getElementById('refs');
-$reference.addEventListener('mouseover', function() {
-  $ref.style.visibility = 'visible'
+const reference = document.getElementById('ref_btn');
+const ref = document.getElementById('refs');
+reference.addEventListener('mouseover', function() {
+  ref.style.visibility = 'visible'
 })
-$reference.addEventListener('mouseout', function() {
-  $ref.style.visibility = 'hidden'
+reference.addEventListener('mouseout', function() {
+  ref.style.visibility = 'hidden'
 })
 
